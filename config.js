f7072b90e796ce46e40215c7a61dd04e2d6dcd94
RGXPK <122319120+RGXPK@users.noreply.github.com>
2026-07-26 19:25:46 +0300

    Add Firebase and Telegram configuration constants

---
+++ config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAhvrYLmyC85YuQaES2QmDfftG9zGkHZAg",
+  authDomain: "upload-d9f13.firebaseapp.com",
+  projectId: "upload-d9f13",
+  storageBucket: "upload-d9f13.firebasestorage.app",
+  messagingSenderId: "450447935996",
+  appId: "1:450447935996:web:0e6ddb4036e49a7c85ca79",
+};
+
+export const TELEGRAM_BOT_TOKEN = "8853388205:AAGsJhSkMXjYVNjYlxiq8gMVNA61sLhNHdM";
+export const TELEGRAM_CHAT_ID = "-1001689016925";
